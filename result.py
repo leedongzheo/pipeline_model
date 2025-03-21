c1e1fc16690069b2ef7bff1d114beeef6940733a
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2025-03-21 17:24:47 +0700

    Update result.py

--- result.py
+++ result.py
@@ -2,7 +2,7 @@
 from config import*
 from train import*
 def export():
-    # output_folder = f"{BASE_OUTPUT}\\output_epoch{trainer.best_epoch}_dice{trainer.avg_val_dice:.4f}"
+
     source_file1='last_model.pth'
     source_file2='best_model.pth'
     path=f"output_epoch{trainer.best_epoch}_dice{trainer.best_dice:.4f}"

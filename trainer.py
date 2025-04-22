9d06cc4ab3e6273437f5f1a5943e86284e8082ff
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2025-04-23 02:35:27 +0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -38,6 +38,7 @@ class Trainer:
         self.checkpoint = torch.load(path, map_location=self.device)
         self.model.load_state_dict(self.checkpoint['model_state_dict'])
         self.optimizer.load_state_dict(self.checkpoint['optimizer_state_dict'])
+        self.start_epoch=self.checkpoint['epoch']
         self.train_losses, self.val_losses = self.checkpoint['train_losses'], self.checkpoint['val_losses']
         self.train_dices, self.val_dices = self.checkpoint['train_dices'], self.checkpoint['val_dices']
         self.best_dice, self.best_epoch = self.checkpoint['best_dice'], self.checkpoint['best_epoch']

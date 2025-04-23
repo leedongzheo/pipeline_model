ab9acbc8256b2aff08914a62ce3bce25222567f2
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2025-04-23 12:12:03 +0700

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -9,8 +9,6 @@ def optimizer(model):
     args = get_args()
     if args.optimizer == "Adam":
         optimizer = Adam(model.parameters(), lr=lr0, weight_decay=weight_decay)
-        # Khởi tạo CosineAnnealingLR scheduler
-        scheduler = CosineAnnealingLR(optimizer, T_max=T_max, eta_min=lr_min)
         return optimizer
     elif args.optimizer == "SGD":
         optimizer = SGD(model.parameters(), lr=lr0, weight_decay=weight_decay) 

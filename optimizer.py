77ee8956f6f32fe992f1e92969044b9b028ebf8f
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2025-04-23 12:09:46 +0700

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -10,8 +10,6 @@ def optimizer(model):
     if args.optimizer == "Adam":
         optimizer = Adam(model.parameters(), lr=lr0, weight_decay=weight_decay)
         # Khởi tạo CosineAnnealingLR scheduler
-        T_max = NUM_EPOCHS  # T_max là số epoch bạn muốn dùng cho giảm lr
-        lr_min = lr0  # lr_min là learning rate tối thiểu
         scheduler = CosineAnnealingLR(optimizer, T_max=T_max, eta_min=lr_min)
         return optimizer
     elif args.optimizer == "SGD":
